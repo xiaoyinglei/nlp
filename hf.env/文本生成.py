706386846083b5fe68aaf9fcc0b243dac02068b5
xiaoyinglei <xiaoyinglei0813@gmail.com>
2025-06-08 15:56:26 +0800

    更新第五章文本生成

--- hf.env/文本生成.py
+++ hf.env/文本生成.py
@@ -37,3 +37,143 @@ with torch.no_grad():
         
 df = pd.DataFrame(iterations)
 print(df)
+
+input_ids = tokenizer(input_txt, return_tensors="pt")["input_ids"].to(device)
+output = model.generate(input_ids, max_new_tokens=n_steps, do_sample=False)
+print(tokenizer.decode(output[0]))
+
+max_length = 128
+input_txt = """In a shocking finding, scientist discovered \
+a herd of unicorns living in a remote, previously unexplored \
+valley, in the Andes Mountains. Even more surprising to the \
+researchers was the fact that the unicorns spoke perfect English.\n\n
+"""
+input_ids = tokenizer(input_txt, return_tensors="pt")["input_ids"].to(device)
+output_greedy = model.generate(input_ids, max_length=max_length, 
+                               do_sample=False)
+print(tokenizer.decode(output_greedy[0]))
+
+import torch.nn.functional as F
+
+def log_probs_from_logits(logits, labels):
+    logp = F.log_softmax(logits, dim=-1)
+    logp_label = torch.gather(logp, 2, labels.unsqueeze(2)).squeeze(-1)
+    return logp_label
+     
+
+def sequence_logprob(model, labels, input_len=0):
+    with torch.no_grad():
+        output = model(labels)
+        log_probs = log_probs_from_logits(
+            output.logits[:, :-1, :], labels[:, 1:])
+        seq_log_prob = torch.sum(log_probs[:, input_len:])
+    return seq_log_prob.cpu().numpy()
+
+logp = sequence_logprob(model, output_greedy, input_len=len(input_ids[0]))
+print(tokenizer.decode(output_greedy[0]))
+print(f"\nlog-prob: {logp:.2f}")
+
+
+output_beam = model.generate(input_ids, max_length=max_length, num_beams=5, 
+                             do_sample=False)
+logp = sequence_logprob(model, output_beam, input_len=len(input_ids[0]))
+print(tokenizer.decode(output_beam[0]))
+print(f"\nlog-prob: {logp:.2f}")
+
+output_beam = model.generate(input_ids, max_length=max_length, num_beams=5, 
+                             do_sample=False, no_repeat_ngram_size=2)
+logp = sequence_logprob(model, output_beam, input_len=len(input_ids[0]))
+print(tokenizer.decode(output_beam[0]))
+print(f"\nlog-prob: {logp:.2f}")
+
+#id temperature
+#alt Token probabilities as a function of temperature
+#caption Distribution of randomly generated token probabilities for three selected temperatures
+import matplotlib.pyplot as plt
+import numpy as np
+
+def softmax(logits, T=1):
+    e_x = np.exp(logits / T)
+    return e_x / e_x.sum()
+
+logits = np.exp(np.random.random(1000))
+sorted_logits = np.sort(logits)[::-1]
+x = np.arange(1000)
+
+for T in [0.5, 1.0, 2.0]:
+    plt.step(x, softmax(sorted_logits, T), label=f"T={T}")
+plt.legend(loc="best")
+plt.xlabel("Sorted token probabilities")
+plt.ylabel("Probability")
+plt.show()
+
+torch.manual_seed(42)
+output_temp = model.generate(input_ids, max_length=max_length, do_sample=True, 
+                             temperature=2.0, top_k=0)
+print(tokenizer.decode(output_temp[0]))
+
+torch.manual_seed(42)
+output_temp = model.generate(input_ids, max_length=max_length, do_sample=True, 
+                             temperature=0.5, top_k=0)
+print(tokenizer.decode(output_temp[0]))
+
+torch.manual_seed(42)
+# hide
+input_txt = """In a shocking finding, scientist discovered \
+a herd of unicorns living in a remote, previously unexplored \
+valley, in the Andes Mountains. Even more surprising to the \
+researchers was the fact that the unicorns spoke perfect English.\n\n
+"""
+input_ids = tokenizer(input_txt, return_tensors="pt")["input_ids"].to(device)
+     
+
+# hide
+import torch.nn.functional as F
+
+with torch.no_grad():
+    output = model(input_ids=input_ids)
+    next_token_logits = output.logits[:, -1, :]
+    probs = F.softmax(next_token_logits, dim=-1).detach().cpu().numpy()
+
+#id distribution
+#alt Probability distribution of next token prediction.
+#caption Probability distribution of next token prediction (left) and cumulative distribution of descending token probabilities
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3.5))
+
+axes[0].hist(probs[0], bins=np.logspace(-10, -1, 100), color="C0", edgecolor="C0")
+axes[0].set_xscale("log")
+axes[0].set_yscale("log")
+axes[0].set_title("Probability distribution")
+axes[0].set_xlabel("Probability")
+axes[0].set_ylabel("Count")
+#axes[0].grid(which="major")
+
+axes[1].plot(np.cumsum(np.sort(probs[0])[::-1]), color="black")
+axes[1].set_xlim([0, 10000])
+axes[1].set_ylim([0.75, 1.01])
+axes[1].set_title("Cumulative probability")
+axes[1].set_ylabel("Probability")
+axes[1].set_xlabel("Token (descending probability)")
+#axes[1].grid(which="major")
+axes[1].minorticks_on()
+#axes[1].grid(which='minor', linewidth='0.5')
+top_k_label = 'top-k threshold (k=2000)'
+top_p_label = 'nucleus threshold (p=0.95)'
+axes[1].vlines(x=2000, ymin=0, ymax=2, color='C0', label=top_k_label)
+axes[1].hlines(y=0.95, xmin=0, xmax=10000, color='C1', label=top_p_label, linestyle='--')
+axes[1].legend(loc='lower right')
+plt.tight_layout()
+     
+torch.manual_seed(42)
+output_topk = model.generate(input_ids, max_length=max_length, do_sample=True, 
+                             top_k=50)
+print(tokenizer.decode(output_topk[0]))
+
+torch.manual_seed(42)
+output_topp = model.generate(input_ids, max_length=max_length, do_sample=True, 
+                             top_p=0.90)
+print(tokenizer.decode(output_topp[0]))
